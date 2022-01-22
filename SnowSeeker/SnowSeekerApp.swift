d1e767ad508a739aa42932815bfcc4691907d892
iOSDevHeath <iOSDevHeath@icloud.com>
2022-01-22 12:37:39 -0500

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Heath Fashina on 2022-01-22.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
