f9fc44ea7c7f0055d7426869c42f8429ee2e8a0f
iOSDevHeath <iOSDevHeath@icloud.com>
2022-01-23 20:56:07 -0500

    Creating a secondary view for NavigationView

--- SnowSeeker/SnowSeekerApp.swift
+++ SnowSeeker/SnowSeekerApp.swift
@@ -12,6 +12,9 @@ struct SnowSeekerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    UserDefaults.standard.setValue(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
+                }
         }
     }
 }
